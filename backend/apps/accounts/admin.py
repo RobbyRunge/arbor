a42bce54ddaad3dcb58c08829e81ae353117b242
RobbyRunge <robbyrunge@yahoo.com>
2026-03-22 22:19:37 +0100

    feat(accounts): add Account model with type, balance, icon and color

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "type", "balance"]
+    list_filter = ["type"]
+    search_fields = ["name", "user__email"]
