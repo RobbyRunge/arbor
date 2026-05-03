781c18a33bf7760cb5ce2b40afd80acb9cc0b33d
RobbyRunge <robbyrunge@yahoo.com>
2026-05-03 23:08:10 +0200

    feat(notifications): update NotificationsPage with placeholder message for upcoming features

--- frontend/src/pages/NotificationsPage.tsx
+++ frontend/src/pages/NotificationsPage.tsx
@@ -1,5 +1,11 @@
 function NotificationsPage() {
-  return <div>Benachrichtigungen</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-gray-800">
+        In Arbeit, funktionalitäten werden demnächst hinzugefügt
+      </h1>
+    </div>
+  );
 }
 
 export default NotificationsPage;
